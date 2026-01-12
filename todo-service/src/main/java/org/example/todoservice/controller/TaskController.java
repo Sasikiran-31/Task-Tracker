fc7f81a0be6a2de0ccf39ea97b7a276cad7d312a
Sasikiran-31 <kirankumar.2001@yahoo.in>
2026-01-11 19:46:46 -0500

    added the controller layer

---
+++ todo-service/src/main/java/org/example/todoservice/controller/TaskController.java
@@ -0,0 +1,17 @@
+package org.example.todoservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/task")
+public class TaskController {
+
+    
+    @GetMapping()
+    public String hello(){
+        return "Hello user";
+    }
+
+}
